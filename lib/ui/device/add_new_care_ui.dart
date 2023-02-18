1acbc858ae98c3bb6650bff1b57877d06f467e72
Nguyễn Hữu Đức <huuduc0804@gmail.com>
2023-02-18 16:04:16 +0700

    Update device from Json

--- lib/ui/device/add_new_care_ui.dart
+++ lib/ui/device/add_new_care_ui.dart
@@ -1,8 +1,37 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz_unsafe.dart';
 import 'package:flutter/material.dart';
 import 'dropdown_custom.dart';
+import 'package:intl/intl.dart';
 
-class AddNewCare extends StatelessWidget {
+class AddNewCare extends StatefulWidget {
   const AddNewCare({super.key});
+
+  @override
+  State<AddNewCare> createState() => _AddNewCareState();
+}
+
+class _AddNewCareState extends State<AddNewCare> {
+  String _device = 'Điện thoại | iPhone 14';
+  String _deviceDetail = 'Màn hình';
+  String _memo = 'Bảo hành màn hình đt';
+  DateTime _date = DateTime.now();
+  TimeOfDay _time = TimeOfDay(
+    hour: DateTime.now().hour,
+    minute: DateTime.now().minute,
+  );
+  String _timeReturn = 'One Time';
+  List<DeviceModel> _listDeviceModel = [];
+
+  @override
+  void initState() {
+    super.initState();
+    var jsonObeject = jsonDecode(_listDeviceJson);
+    _listDeviceModel = jsonObeject
+        .map<DeviceModel>((json) => DeviceModel.fromJson(json))
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,30 +81,34 @@ class AddNewCare extends StatelessWidget {
                       style:
                           TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
                     ),
-                    Image.asset(
-                      'assets/images/icon_qr.png',
-                      width: 14,
-                      height: 14,
-                    )
+                    Image.asset('assets/images/icon_qr.png',
+                        width: 14, height: 14),
                   ],
                 ),
               ),
               const SizedBox(height: 4),
               DropdownDeviceCustom(
-                text: 'Điện thoại | Iphone 14',
-                image: Image.asset('assets/images/drugs.png'),
-                isDropdown: true,
-                func: () {},
-              ),
+                  text: _device,
+                  image: Image.asset('assets/images/drugs.png'),
+                  isDropdown: true,
+                  func: () async {
+                    var value =
+                        await _showButtomListExpand(context, _listDeviceModel);
+                    setState(() {
+                      _device = value;
+                    });
+                  }),
               const SizedBox(height: 8),
               DropdownDeviceCustom(
-                text: 'Màn hình',
-                image: const Icon(
-                  Icons.format_list_bulleted_sharp,
-                  size: 14,
-                ),
+                text: _deviceDetail,
+                image: const Icon(Icons.format_list_bulleted_sharp, size: 14),
                 isDropdown: true,
-                func: () {},
+                func: () async {
+                  var value = await _showButtomList(context, _listDeviceDetail);
+                  setState(() {
+                    _deviceDetail = value;
+                  });
+                },
               ),
               const SizedBox(height: 10),
               Container(
@@ -88,13 +121,14 @@ class AddNewCare extends StatelessWidget {
               ),
               const SizedBox(height: 4),
               DropdownDeviceCustom(
-                text: 'Bảo hành màn hình đt',
-                image: const Icon(
-                  Icons.more_horiz_sharp,
-                  size: 14,
-                ),
-                isDropdown: false,
-                func: () {},
+                text: _memo,
+                image: const Icon(Icons.more_horiz_sharp, size: 14),
+                func: () async {
+                  var value = await _showButtomList(context, _listMemo);
+                  setState(() {
+                    _memo = value;
+                  });
+                },
               ),
               const SizedBox(height: 18),
               const Text(
@@ -113,21 +147,46 @@ class AddNewCare extends StatelessWidget {
                   Expanded(
                     flex: 1,
                     child: DropdownDeviceCustom(
-                      text: '  21/12/2022',
+                      text: DateFormat('dd/MM/yyyy').format(_date).toString(),
                       image:
                           const Icon(Icons.calendar_month_outlined, size: 14),
-                      isDropdown: false,
-                      func: () {},
+                      isSmall: true,
+                      func: () async {
+                        final DateTime? picked = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(DateTime.now().year - 2),
+                          lastDate: DateTime(DateTime.now().year + 2),
+                        );
+                        if (picked != null && picked != _date) {
+                          setState(() {
+                            _date = picked;
+                          });
+                        }
+                      },
                     ),
                   ),
                   const SizedBox(width: 18),
                   Expanded(
                     flex: 1,
                     child: DropdownDeviceCustom(
-                      text: '   10:00 AM',
+                      text: _time.format(context).toString(),
                       image: const Icon(Icons.more_time_sharp, size: 14),
-                      isDropdown: false,
-                      func: () {},
+                      isSmall: true,
+                      func: () async {
+                        final TimeOfDay? picker = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay(
+                            hour: DateTime.now().hour,
+                            minute: DateTime.now().minute,
+                          ),
+                        );
+                        if (picker != null && picker != _time) {
+                          setState(() {
+                            _time = picker;
+                          });
+                        }
+                      },
                     ),
                   ),
                 ],
@@ -138,19 +197,26 @@ class AddNewCare extends StatelessWidget {
                   Expanded(
                     flex: 1,
                     child: DropdownDeviceCustom(
-                      text: '  One Time',
+                      text: _timeReturn,
                       image: const Icon(Icons.av_timer_outlined, size: 14),
                       isDropdown: true,
-                      func: () {},
+                      isSmall: true,
+                      func: () async {
+                        var value = await _showButtomList(context, _listTime);
+                        setState(() {
+                          _timeReturn = value;
+                        });
+                      },
                     ),
                   ),
                   const SizedBox(width: 18),
                   Expanded(
                     flex: 1,
                     child: DropdownDeviceCustom(
-                      text: '   30   days',
+                      text: '30 \t\t\t days',
                       image: const Icon(Icons.wallet_travel_rounded, size: 14),
                       isDropdown: true,
+                      isSmall: true,
                       func: () {},
                     ),
                   ),
@@ -167,7 +233,7 @@ class AddNewCare extends StatelessWidget {
               ),
               const SizedBox(height: 4),
               GestureDetector(
-                onTap: (() {}),
+                onTap: () async {},
                 child: Image.asset('assets/images/upload_image.png'),
               ),
               const SizedBox(height: 27),
@@ -187,11 +253,148 @@ class AddNewCare extends StatelessWidget {
                           color: Colors.white)),
                 ),
               ),
-              const SizedBox(height: 26)
+              const SizedBox(height: 26),
             ],
           ),
         ),
       )),
     );
+  }
+}
+
+Future<String> _showButtomListExpand(
+    BuildContext context, List<DeviceModel> listDeviceDetail) async {
+  String result = '';
+  await showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return ListView(
+          shrinkWrap: true,
+          children: listDeviceDetail.map((value) {
+            return ExpansionTile(
+              title: Text(value.device!),
+              leading: const Icon(Icons.devices_other_sharp),
+              children: value.deviceDetail!.map((e) {
+                return ListTile(
+                    title: Text(e.name!),
+                    onTap: () {
+                      result = '${value.device!} | ${e.name!}';
+                      Navigator.pop(context);
+                    });
+              }).toList(),
+            );
+          }).toList(),
+        );
+      });
+  return result;
+}
+
+Future<String> _showButtomList(BuildContext context, List<String> list) async {
+  String result = '';
+  await showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return ListView(
+          children: list.map((value) {
+            return ListTile(
+              title: Text(value),
+              onTap: () {
+                result = value;
+                Navigator.pop(context);
+              },
+            );
+          }).toList(),
+        );
+      });
+  return result;
+}
+
+List<String> _listDeviceDetail = [
+  'Màn hình',
+  'Bàn phím',
+  'Loa ngoài',
+];
+List<String> _listMemo = [
+  'Bảo hành màn hình đt',
+  'Bảo hành bàn phím',
+  'Bảo hành loa ngoài',
+];
+List<String> _listTime = [
+  'One Time',
+  'Two Time',
+  'Three Time',
+];
+var _listDeviceJson = '''
+[
+    {
+        "device": "Điện thoại",
+        "device_detail": [
+            {
+                "name": "iPhone 14"
+            },
+            {
+                "name": "iPhone 14 Pro Max"
+            },
+            {
+                "name": "Samsung"
+            }
+        ]
+    },
+    {
+        "device": "Máy tính",
+        "device_detail": [
+            {
+                "name": "Máy tính bàn"
+            },
+            {
+                "name": "Máy tính xách tay"
+            },
+            {
+                "name": "Máy tính đồng bộ"
+            }
+        ]
+    }
+]
+''';
+
+class DeviceModel {
+  String? device;
+  List<DeviceDetail>? deviceDetail;
+
+  DeviceModel({this.device, this.deviceDetail});
+
+  DeviceModel.fromJson(Map<String, dynamic> json) {
+    device = json['device'];
+    if (json['device_detail'] != null) {
+      deviceDetail = <DeviceDetail>[];
+      json['device_detail'].forEach((v) {
+        deviceDetail!.add(DeviceDetail.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['device'] = device;
+    if (deviceDetail != null) {
+      data['device_detail'] = deviceDetail!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class DeviceDetail {
+  String? name;
+
+  DeviceDetail({this.name});
+
+  DeviceDetail.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    return data;
   }
 }
--- lib/ui/device/dropdown_custom.dart
+++ lib/ui/device/dropdown_custom.dart
@@ -4,19 +4,23 @@ class DropdownDeviceCustom extends StatelessWidget {
   final Widget image;
   final String text;
   final bool isDropdown;
+  final bool isSmall;
   final Function() func;
-  const DropdownDeviceCustom(
-      {Key? key,
-      required this.text,
-      required this.func,
-      required this.image,
-      required this.isDropdown})
-      : super(key: key);
+  const DropdownDeviceCustom({
+    Key? key,
+    required this.text,
+    required this.image,
+    this.isDropdown = false,
+    this.isSmall = false,
+    required this.func,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double paddingLeft = isSmall ? 24 : 18;
+    double paddingRight = isSmall ? 14 : 9;
     return Container(
-        padding: const EdgeInsets.only(left: 18, right: 21),
+        padding: EdgeInsets.only(left: paddingLeft, right: 21),
         height: 48,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(14),
@@ -26,21 +30,15 @@ class DropdownDeviceCustom extends StatelessWidget {
           child: Row(
             children: [
               image,
-              const SizedBox(width: 9),
+              SizedBox(width: paddingRight),
               Expanded(
-                  child: Text(
-                text,
-                style: const TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                ),
-              )),
+                child: Text(text,
+                    style: const TextStyle(
+                        fontSize: 15, fontWeight: FontWeight.w500)),
+              ),
               isDropdown
-                  ? const Icon(
-                      Icons.arrow_drop_down_rounded,
-                      color: Color(0xFF9B9B9B),
-                      size: 15,
-                    )
+                  ? const Icon(Icons.arrow_drop_down_rounded,
+                      color: Color(0xFF9B9B9B), size: 15)
                   : const SizedBox()
             ],
           ),
